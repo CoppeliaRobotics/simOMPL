24307f1731e5d1ac2723d9d91942606971079275
Federico Ferri <federico.ferri.it@gmail.com>
2020-05-04 15:46:09 +0200

    use module info api

--- plugin.cpp
+++ plugin.cpp
@@ -1708,8 +1708,8 @@ public:
         if(!registerScriptStuff())
             throw std::runtime_error("failed to register script stuff");
 
-        simSetModuleInfo(PLUGIN_NAME, 0, "OMPL (open motion planning library) Plugin", 0);
-        simSetModuleInfo(PLUGIN_NAME, 1, BUILD_DATE, 0);
+        setExtVersion("OMPL (open motion planning library) Plugin");
+        setBuildDate(BUILD_DATE);
     }
 
     void onSimulationEnded()
